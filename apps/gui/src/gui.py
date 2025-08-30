8b3c2b87f33380c062eba260c678064cf82bdc24
fubblea <ajay.arthur@gmail.com>
2025-08-30 16:25:21 -0400

    Added lib dep

--- apps/gui/src/gui.py
+++ apps/gui/src/gui.py
@@ -1,4 +1,5 @@
 import numpy as np
+from compressor.tools import compress
 from nicegui import ui
 
 
@@ -6,6 +7,7 @@ def main():
     arr = np.array([1, 2, 3, 4, 5])
     ui.label(f"Numpy array: {arr}")
     ui.label(f"Array mean: {np.mean(arr)}")
+    ui.label(f"Compressed string: '{compress('  Hello   World  ')}'")
 
 
 if __name__ in {"__main__", "__mp_main__"}:
