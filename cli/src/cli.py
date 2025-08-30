499628af274ffa8bb5c174da4c4dc322121293d6
fubblea <ajay.arthur@gmail.com>
2025-08-30 15:29:57 -0400

    Setup structure

---
+++ cli/src/cli.py
@@ -0,0 +1,6 @@
+def main():
+    print("This is the CLI")
+
+
+if __name__ == "__main__":
+    main()
---
+++ libs/compressor/src/compressor/tools.py
@@ -0,0 +1,3 @@
+def compress(data: str) -> str:
+    """Compress the input string by removing whitespace."""
+    return "".join(data.split())
